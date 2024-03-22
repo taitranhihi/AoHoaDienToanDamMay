1346e42178f414bf2ea8aa20fab2dbdc86300347
taitranhihi <150355204+taitranhihi@users.noreply.github.com>
2024-03-22 08:36:24 +0700

    BaiTap2.cpp

--- BaiTap2.cpp
+++ BaiTap2.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-// Hàm kiểm tra xem một số có phải là số chính phương hay không
+
 int isPerfectSquare(int n) {
     int squareRoot = sqrt(n);
     return squareRoot * squareRoot == n;
 }
 
-// Hàm đếm và in ra các số chính phương nhỏ hơn n
 void countAndPrintPerfectSquares(int n) {
     printf("Cac so chinh phuong nho hon %d la:\n", n);
     for (int i = 1; i < n; i++) {
